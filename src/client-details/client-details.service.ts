808ffae73e665eb842f446ef022472196ea5435a
Rockersooraz <rockersooraz80@gmail.com>
2021-05-22 10:14:04 +0545

    feat: pagination added

--- src/client-details/client-details.service.ts
+++ src/client-details/client-details.service.ts
@@ -1,60 +1,66 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { ClientDto } from '../dto/client.dto';
-import { CsvWriterUtils } from './utils/csv-writer-utils';
-import { CsvReaderUtils } from './utils/csv-reader-utils';
-
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { ClientDto } from '../dto/client.dto'
+import { CsvWriterUtils } from './utils/csv-writer-utils'
+import { CsvReaderUtils } from './utils/csv-reader-utils'
+import { PageableDto } from '../dto/pageable.dto'
 @Injectable()
 export class ClientDetailsService {
-  findAll(): ClientDto[] {
-    return CsvReaderUtils.getJsonStoredData();
-  }
 
-  findByClientId(clientId: string): ClientDto {
-    if (CsvReaderUtils.findClientId(clientId) === false) {
-      throw new BadRequestException(
-        "Can't find client with Provided clientId",
-        clientId,
-      );
-    }
-    const client = CsvReaderUtils.getJsonStoredData();
-    return client.find((client) => client.id === clientId);
-  }
+	findAll(query: number): PageableDto {
+		const client = CsvReaderUtils.getJsonStoredData();
+		return {
+			client: client.slice(0, query + 1),
+			limit: query,
+			page: Math.ceil(client.length / query)
+		}
+	}
 
-  create(clientInfo: ClientDto): ClientDto {
-    if (CsvReaderUtils.findClientId(clientInfo.id) === true) {
-      throw new BadRequestException('Client Already existed', clientInfo.id);
-    }
-    const data = CsvReaderUtils.getJsonStoredData();
-    const writeIntoFile = new CsvWriterUtils();
-    writeIntoFile.writeToCsvFormat([...data, clientInfo]);
-    return clientInfo;
-  }
+	findByClientId(clientId: string): ClientDto {
+		if (CsvReaderUtils.findClientId(clientId) === false) {
+			throw new BadRequestException(
+				"Can't find client with Provided clientId",
+				clientId
+			)
+		}
+		const client = CsvReaderUtils.getJsonStoredData()
+		return client.find((client) => client.id === clientId)
+	}
 
-  update(userInfo: ClientDto, clientId: string): ClientDto {
-    if (CsvReaderUtils.findClientId(clientId) === false) {
-      throw new BadRequestException("Can't update client", clientId);
-    }
-    const client = CsvReaderUtils.getJsonStoredData();
-    const clientIndex = client.findIndex((user) => user.id === clientId);
-    client[clientIndex] = userInfo;
-    const writeIntoFile = new CsvWriterUtils();
-    writeIntoFile.writeToCsvFormat([...client]);
-    CsvReaderUtils.readCsvFile();
-    return userInfo;
-  }
+	create(clientInfo: ClientDto): ClientDto {
+		if (CsvReaderUtils.findClientId(clientInfo.id) === true) {
+			throw new BadRequestException('Client Already existed', clientInfo.id)
+		}
+		const data = CsvReaderUtils.getJsonStoredData()
+		const writeIntoFile = new CsvWriterUtils()
+		writeIntoFile.writeToCsvFormat([...data, clientInfo])
+		return clientInfo
+	}
 
-  delete(clientId: string): string {
-    if (CsvReaderUtils.findClientId(clientId) === false) {
-      throw new BadRequestException(
-        `Client having client-id ${clientId} doesn't exist`,
-      );
-    }
-    const client = CsvReaderUtils.getJsonStoredData().filter(
-      (data) => data.id !== clientId,
-    );
-    const writeIntoFile = new CsvWriterUtils();
-    writeIntoFile.writeToCsvFormat([...client]);
-    CsvReaderUtils.readCsvFile();
-    return clientId;
-  }
+	update(userInfo: ClientDto, clientId: string): ClientDto {
+		if (CsvReaderUtils.findClientId(clientId) === false) {
+			throw new BadRequestException("Can't update client", clientId)
+		}
+		const client = CsvReaderUtils.getJsonStoredData()
+		const clientIndex = client.findIndex((user) => user.id === clientId)
+		client[clientIndex] = userInfo
+		const writeIntoFile = new CsvWriterUtils()
+		writeIntoFile.writeToCsvFormat([...client])
+		CsvReaderUtils.readCsvFile()
+		return userInfo
+	}
+
+	delete(clientId: string): string {
+		if (CsvReaderUtils.findClientId(clientId) === false) {
+			throw new BadRequestException(
+				`Client having client-id ${clientId} doesn't exist`
+			)
+		}
+		const client = CsvReaderUtils.getJsonStoredData().filter(
+			(data) => data.id !== clientId
+		)
+		const writeIntoFile = new CsvWriterUtils()
+		writeIntoFile.writeToCsvFormat([...client])
+		CsvReaderUtils.readCsvFile()
+		return clientId
+	}
 }
---
+++ src/dto/pageable.dto.ts
@@ -0,0 +1,7 @@
+import {ClientDto} from "./client.dto";
+
+export class PageableDto {
+    readonly client: ClientDto[];
+    readonly limit: number;
+    readonly page: number;
+}
